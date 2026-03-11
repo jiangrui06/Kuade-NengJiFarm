831554060e8a3997a07fe164acce332c4a02209c
刘浩林 <1038006735@qq.com>
2026-03-11 15:05:55 +0800

    1

--- WebApi/WebAPI.net7/obj/Debug/net7.0/WebAPI 2.AssemblyInfo.cs
+++ WebApi/WebAPI.net7/obj/Debug/net7.0/WebAPI 2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7dacf370a15621665a967ee788435d4030e11a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0048693e6e6880dcc5bad3f9eb6dda313207f60c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI 2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
