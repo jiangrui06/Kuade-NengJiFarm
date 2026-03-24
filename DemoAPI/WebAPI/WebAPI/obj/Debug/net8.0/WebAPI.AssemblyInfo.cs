48ef25883f971c4b160bc40c096e14a66d2ea1d6
许乐轩 <519269623@qq.com>
2026-03-24 15:31:15 +0800

    111

--- DemoAPI/WebAPI/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ DemoAPI/WebAPI/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a875cb1ac6e23764b97368324cdf943f21956d1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+312a4e993722780bbe971136dce01f23ade6cf20")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
