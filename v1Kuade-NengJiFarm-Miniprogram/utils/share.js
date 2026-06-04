fb35b264775b0ac68eeaf24346e279affc2a6275
jiangrui <2492968109@qq.com>
2026-06-04 17:37:23 +0800

    修复了分享没有数据的bug

--- v1Kuade-NengJiFarm-Miniprogram/utils/share.js
+++ v1Kuade-NengJiFarm-Miniprogram/utils/share.js
@@ -6,9 +6,10 @@ const SHARE_TITLE = '稻田时光农场 · 回归自然 享受生活';
 
 module.exports = {
   onShareAppMessage() {
+    const query = Object.entries(this.options || {}).map(([k, v]) => `${k}=${v}`).join('&');
     return {
       title: SHARE_TITLE,
-      path: this.route,
+      path: this.route + (query ? '?' + query : ''),
     };
   },
 
